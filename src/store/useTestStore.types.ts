fffee36b751f7a7413df0ccbae3180f9ba8ed367
wilmar miguez <wilmarfm156@hotmail.com>
2025-09-22 16:20:18 -0500

    Feat: Agregada funcionalidad al boton de editar

--- src/store/useTestStore.types.ts
+++ src/store/useTestStore.types.ts
@@ -6,6 +6,7 @@ export interface TestStore {
   tests: Test[];
   addTest: (test: Test) => void;
   removeTest: (index: number) => void;
+  updateTest: (index: number, test: Test) => void;
   clearTests: () => void;
   isBlocked: boolean;
   blockForm: () => void;
