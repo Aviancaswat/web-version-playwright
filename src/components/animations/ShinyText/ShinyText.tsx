880499525ab34f145abc37f799a982f985cbb649
faidev <fainnerramirez@gmail.com>
2025-10-22 13:31:39 -0500

    feat: se agrega estilos de carga texto

--- src/components/animations/ShinyText/ShinyText.tsx
+++ src/components/animations/ShinyText/ShinyText.tsx
@@ -4,13 +4,14 @@ type SninyTextProps = {
   text: string,
   disabled?: boolean,
   speed?: number,
-  className?: string
+  className?: string,
+  color?: string
 }
 
-const ShinyTextAnimation: React.FC<SninyTextProps> = ({ text, disabled = false, speed = 5, className = '' }) => {
+const ShinyTextAnimation: React.FC<SninyTextProps> = ({ text, disabled = false, speed = 5, className = '', color }) => {
   const animationDuration = `${speed}s`;
   return (
-    <div className={`shiny-text ${disabled ? 'disabled' : ''} ${className}`} style={{ animationDuration, color: "hsla(0, 11%, 77%, 0.42)" }}>
+    <div className={`shiny-text ${disabled ? 'disabled' : ''} ${className}`} style={{ animationDuration, color: color || "hsla(0, 11%, 77%, 0.42)" }}>
       {text}
     </div>
   );
---
+++ src/components/animations/agent-dashboard/shinyEffectComponent.tsx
@@ -0,0 +1,13 @@
+import './shinyEffect.css';
+
+const ShinyTextAgent = ({ text = "", disabled = false, speed = 2, className = '' }) => {
+  const animationDuration = `${speed}s`;
+
+  return (
+    <div className={`shiny-text ${disabled ? 'disabled' : ''} ${className}`} style={{ animationDuration, backgroundColor: "black", fontWeight: "bold", padding: "5px 10px", borderRadius: "5px" }}>
+      {text}
+    </div>
+  );
+};
+
+export default ShinyTextAgent;
