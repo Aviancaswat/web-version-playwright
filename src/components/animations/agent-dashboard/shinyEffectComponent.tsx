45f894bb432a11bbfc9f3be651a21c45d15a44c9
faidev <fainnerramirez@gmail.com>
2025-10-22 13:55:10 -0500

    feat: se agrega copiar respuesta funcion

--- src/components/animations/agent-dashboard/shinyEffectComponent.tsx
+++ src/components/animations/agent-dashboard/shinyEffectComponent.tsx
@@ -4,7 +4,7 @@ const ShinyTextAgent = ({ text = "", disabled = false, speed = 2, className = ''
   const animationDuration = `${speed}s`;
 
   return (
-    <div className={`shiny-text ${disabled ? 'disabled' : ''} ${className}`} style={{ animationDuration, backgroundColor: "black", fontWeight: "bold", padding: "5px 10px", borderRadius: "5px" }}>
+    <div className={`shiny-text ${disabled ? 'disabled' : ''} ${className}`} style={{ animationDuration, backgroundColor: "gray", fontWeight: "bold", padding: "5px 10px", borderRadius: "5px" }}>
       {text}
     </div>
   );
